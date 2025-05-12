2008bdc4cf5e449322e0c967f4a3273c414f4888
Mohammed Achref Hemissi <110178894+AchrefHemissi@users.noreply.github.com>
2025-05-12 18:43:51 +0100

    link the gateway with the proxy

--- gateway/app/services/proxy/client.py
+++ gateway/app/services/proxy/client.py
@@ -1,12 +1,16 @@
+from fastapi import HTTPException
 import requests
 
-PROXY_URL = "http://localhost:8002/upload/"
+PROXY_URL = "http://127.0.0.1:8005/upload/"
 
-def send_image(file):
+async def send_image(file, x_api_key, image_bytes):
     try:
-        files = {"file": (file.filename, file.file, file.content_type)}
-        response = requests.post(PROXY_URL, files=files)
+ 
+        files = {"file": (file.filename, image_bytes, file.content_type)}
+        response = requests.post(PROXY_URL, files=files, headers={"x-api-key": x_api_key}) 
         return response.json()
     except Exception as e:
         print(f"Error sending image to proxy: {e}")
-        return {"error": str(e)}
+        #return {"error": str(e)}
+        raise HTTPException(status_code=500, detail=str(e))
+    
